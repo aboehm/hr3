b83af3f55b18233d9d5d5560487270bc42abef7e
Alexander Böhm <mai12lhv@studserv.uni-leipzig.de>
2014-05-19 19:52:27 +0200

    add script for standard test matrix creation

--- scripts/test-matrix.sh
+++ scripts/test-matrix.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-for dim in `seq 1 5` ; do
-for points in 10000 100000 1000000 ; do 
-for range in 1 10 90 180 ; do
-for merge in 0 1 ; do
+export THREADS="2 4 5 8 9 16 17 64 128 256"
+export JAVA_OPTS="-Xmx8G"
+JAVA=java
 
-DO_MERGE=""
-[ $merge -eq 1 ] && DO_MERGE="--mapmerge"
-
-for threads in 1 2 4 5 6 7 8 9 10 16 32 64 128 ; do
-	java -jar build/jar/HR3.jar --benchmark \
+run_bench() {
+	echo $JAVA $JAVA_OPTS \
+		 -jar build/jar/HR3.jar \
+		--benchmark \
 		--memory \
-		--threads $threads \
 		--random \
-		--random-points $points \
-		--random-dimensions $dim \
-		--random-range-begin 0.0 \
-		--random-range-end $range \
-		$DO_MERGE
-done
-done
-done
-done
-done
+		--random-points $1 \
+		--random-dimensions $2 \
+		--random-range-begin $3 \
+		--random-range-end $4 \
+		--benchmark-header
+
+	for p in 0 1 ; do
+		DO_EXTREMEPOOL=""
+		[ $p -eq 1 ] && DO_EXTREMEPOOL="--extremepool"
+
+		for m in 0 1 ; do
+			DO_MERGE=""
+			[ $m -eq 1 ] && DO_MERGE="--mapmerge"
+
+			for t in $THREADS ; do
+				echo $JAVA $JAVA_OPTS \
+					 -jar build/jar/HR3.jar \
+					--benchmark \
+					--memory \
+					--threads $t \
+					--random \
+					--random-points $1 \
+					--random-dimensions $2 \
+					--random-range-begin $3 \
+					--random-range-end $4 \
+					$DO_MERGE $DO_EXTREMEPOOL
+			done
+		done
+	done
+}
+
+run_bench 6000 3 0 1
+run_bench 10000 3 0 10
+run_bench 100000 3 0 45
+run_bench 150000 3 0 90
+run_bench 300000 3 0 180
 
